4fbf223fa6473622a4d8817538ef41a20a332b4a
Daniela Soto <dz0597@gmail.com>
2022-07-18 22:39:24 -0500

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+counties= ["Arapahoe","Denver","Jefferson"]
+if counties[1] == "Denver":
+    print(counties[1])
+
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+     print("Turn on the AC.")
+else:
+     print("Open the windows.")
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+     print("El Paso is in the list of counties.")
+else:
+     print("El Paso is not the list of counties.")
+
+ 
+if "Arapahoe" in counties or "El Paso" in counties:
+     print("Arapahoe and El Paso are in the list of counties.")
+else:
+     print("Arapahoe or El Paso is not in the list of counties.")
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+#what is the highest total order price (total product price plus total shipping price) to the nearest cent?
+
